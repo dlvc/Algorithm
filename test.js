6a2767f10fea5ff45f0fad033861845005b3874d
dlvc <1529616991@qq.com>
2022-04-16 13:29:31 +0800

    二叉树深度搜索

--- test.js
+++ test.js
@@ -1,43 +1,30 @@
-function Node(value) {
+function Tree(value) {
     this.value = value;
-    this.next = null;
+    this.left = null;
+    this.right = null;
 }
 
-const a = new Node('1');
-const b = new Node('2');
-const c = new Node('3');
-const d = new Node('4');
-const e = new Node('5');
-const f = new Node('6');
-const g = new Node('7');
+const a = new Tree('a');
+const b = new Tree('b');
+const c = new Tree('c');
+const d = new Tree('d');
+const e = new Tree('e');
+const f = new Tree('f');
+const g = new Tree('g');
 
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-e.next = f;
-f.next = g;
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.left = f;
+c.right = g;
 
-function nizhi(root) {
-    if (root.next.next == null) {
-        root.next.next = root;
-        return root.next;
-    }else {
-        const result = nizhi(root.next);
-        root.next.next = root;
-        root.next = null;
-        return result;
-    }
-}
+function deepSearch(root, target) {
+    if (root == null) return false;
+    if (root.value == target) return true;
+    const left = deepSearch(root.left, target);
+    const right = deepSearch(root.right, target);
+    return left || right;
+} 
 
-// nizhi(a);
-
-function bianli(root) {
-    if (root == null) {
-        return ;
-    }
-    console.log(root.value);
-    bianli(root.next);
-}
-
-bianli(nizhi(a));
+console.log(deepSearch(a, 'x'))
